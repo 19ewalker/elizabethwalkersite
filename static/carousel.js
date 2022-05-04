6604a0b40bbf7b2a18f116d14ab6a12c737f63c9
19ewalker <44076192+19ewalker@users.noreply.github.com>
2022-05-04 13:02:42 -0400

    added background color, fade in, changing column sizes

---
+++ static/carousel.js
@@ -0,0 +1,14 @@
+/* Carousel */
+(function() {
+	var aboutTyped = new Typed('#iam-carousel', {
+		strings: ["hi there, i'm libby :-)"],
+		typeSpeed: 120,
+		startDelay: 200,
+		fadeOut: false,
+		loop: false,
+		showCursor: false,
+	});
+});
+
+/* AOS */
+AOS.init({ duration: 1000, once: true });
